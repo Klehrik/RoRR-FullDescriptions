72778d28a146ea4e5a1d8a4a0ac23e2765f701b9
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-05-22 17:02:06 -0400

    v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 
 ### v1.0.1
 * Updated description
+
+### v1.0.2
+* Items without a long description are now unaffected.
+* Now inserts line breaks into appropriate spots.
--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
--- Full Descriptions v1.0.1
+-- Full Descriptions v1.0.2
 -- Klehrik
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
+
+local line_length = 80
 
 
 
@@ -15,8 +17,26 @@ gm.pre_script_hook(gm.constants.run_create, function()
         local class = gm.variable_global_get(c)
         for i = 1, #class do
             local item = class[i]
-            local localization = string.sub(item[3], 1, #item[3] - 5)
-            gm.ds_map_set(lang_map, localization..".pickup", gm.ds_map_find_value(lang_map, localization..".description"))
+            local localization = string.sub(item[3], 1, #item[3] - 5)   -- "item.<identifier>"
+            local desc = gm.ds_map_find_value(lang_map, localization..".description")
+
+            if desc then
+                -- Insert breakpoints every line_length chars after a space
+                local formatted = ""
+                local current = 1
+                for i = line_length, #desc, line_length do
+                    for j = i, #desc do
+                        if string.sub(desc, j, j) == " " then
+                            formatted = formatted..string.sub(desc, current, j).."\n"
+                            current = j + 1
+                            break
+                        end
+                    end
+                end
+                formatted = formatted..string.sub(desc, current, #desc)
+
+                gm.ds_map_set(lang_map, localization..".pickup", formatted)
+            end
         end
     end
 end)
